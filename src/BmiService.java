f47eadaf7eeed9fb6e3dc3e71cbde317f3ef34af
Anton Osychenko <osychenkoanton@gmail.com>
2023-04-28 04:08:33 +0300

    end commit

---
+++ src/BmiService.java
@@ -0,0 +1,8 @@
+public class BmiService {
+    public int calculate(double weightKg, double heightMetre) {
+
+        double result = weightKg / (heightMetre * heightMetre);
+
+        return (int) result;
+    }
+}
